b59616a718bc3b711aa249919ffa80af31ca3e8d
Kev <kevinhatran@utexas.edu>
2023-07-26 14:20:45 -0500

    no further studies done

---
+++ src/Pokecard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Pokecard.css'
+const POKE_API = 'https://raw.githubusercontent.com/' +
+    'PokeAPI/sprites/master/sprites/pokemon/';
+
+const Pokecard = (props) => {
+    let imgSrc = `${POKE_API}${props.id}.png`;
+
+    return (
+        <div className="Pokecard">
+            <div className="Pokecard-title">
+                {props.name}
+            </div>
+            <img className="Pokecard-img" src={imgSrc} />
+            <div className="Pokecard-data">
+                Type: {props.type}
+            </div>
+            <div className="Pokecard-data">
+                EXP pts: {props.exp}
+            </div>
+
+
+
+
+        </div>
+    )
+}
+
+export default Pokecard;
